83ef930f85aed3392e6415f5b69a51fd222406f2
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-09-30 15:52:32 +0545

    feat: maintenance  page  responsiveness added

--- src/components/custom-components/maintenance.tsx
+++ src/components/custom-components/maintenance.tsx
@@ -7,52 +7,54 @@ const Maintenance = () => {
   const { smallHeading, largeHeading, cards } = cardData;
 
   return (
-    <section className="container flex flex-row justify-center min-h-[100vh] max-w-[100vw] bg-customColor-lightGrey border border-red-500">
-      <div className="flex flex-col justify-center items-center gap-8 my-10">
-        <p className="text-t2 font-bold text-center p-1">{smallHeading}</p>
-        <h2 className="text-h2 font-extrabold max-w-[858px] leading-tight text-center">
-          {largeHeading}
-        </h2>
-        <div className="flex flex-row justify-center items-center flex-wrap gap-2 w-[80%]">
-          {cards.map((card, index) => (
-            <Card
-              key={index}
-              className="group flex flex-col justify-center items-center w-[316px] h-[232px] rounded-sm hover:bg-customColor-primary hover:cursor-pointer"
-            >
-              <CardHeader>
-                <CardTitle className="flex justify-center items-center">
+    <div className="bg-customColor-lightGrey">
+      <section className="container flex flex-row justify-center min-h-[100vh] max-w-[100vw] bg-customColor-lightGrey ">
+        <div className="   flex flex-col justify-center items-center gap-8 my-10">
+          <p className="text-t2 font-bold text-center p-1">{smallHeading}</p>
+          <h2 className="text-h2 font-extrabold max-w-[858px] leading-tight text-center">
+            {largeHeading}
+          </h2>
+          <div className="flex flex-row justify-center items-center flex-wrap gap-2 w-[80%]">
+            {cards.map((card, index) => (
+              <Card
+                key={index}
+                className="group flex flex-col justify-center items-center w-[316px] h-[232px] rounded-sm hover:bg-customColor-primary hover:cursor-pointer"
+              >
+                <CardHeader>
+                  <CardTitle className="flex justify-center items-center">
+                    <Image
+                      src={card.image}
+                      width={44}
+                      height={49}
+                      className="group-hover:brightness-0 group-hover:invert"
+                      alt={`${card.title} Icon`}
+                    />
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p className="text-center text-t2 font-semibold group-hover:text-customColor-white">
+                    {card.title}
+                  </p>
+                </CardContent>
+              </Card>
+            ))}
+            <div className="w-[84%] pt-2">
+              <div className="flex flex-row items-center justify-end gap-6 hover:cursor-pointer">
+                <p className="text-b1 font-semibold">Know more about us</p>
+                <span className="">
                   <Image
-                    src={card.image}
-                    width={44}
-                    height={49}
-                    className="group-hover:brightness-0 group-hover:invert"
-                    alt={`${card.title} Icon`}
+                    src="/project/right-arrow.svg"
+                    width={52}
+                    height={0}
+                    alt="Right arrow"
                   />
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p className="text-center text-t2 font-semibold group-hover:text-customColor-white">
-                  {card.title}
-                </p>
-              </CardContent>
-            </Card>
-          ))}
-          <div className="w-[84%] pt-2">
-            <div className="flex flex-row items-center justify-end gap-6 hover:cursor-pointer">
-              <p className="text-b1 font-semibold">Know more about us</p>
-              <span className="">
-                <Image
-                  src="/project/right-arrow.svg"
-                  width={52}
-                  height={0}
-                  alt="Right arrow"
-                />
-              </span>
+                </span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
