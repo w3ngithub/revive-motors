49c3949c06f778b39e7d0bfa4cd620fbe501936a
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-09-30 11:02:53 +0545

    feat: customer-satisfaction  section  made dynamic

--- src/components/custom-components/customer-satisfaction.tsx
+++ src/components/custom-components/customer-satisfaction.tsx
@@ -1,33 +1,21 @@
 import React from "react";
 
+import customerSatisfactionData from "../../json/customer-satisfaction.json";
+
 const CustomerSatisfaction = () => {
   return (
     <section className="bg-customColor-lightGrey ">
       <div className="container flex flex-row justify-between items-center py-8 flex-wrap ">
-        <div className="  ">
-          <h2 className="text-h2 text-customColor-secondary font-extrabold">
-            20+
-          </h2>
-          <p className="text-t2 font-semibold">Professional</p>
-        </div>
-        <div className="  ">
-          <h2 className="text-h2 text-customColor-secondary font-extrabold">
-            10+
-          </h2>
-          <p className="text-t2 font-semibold">Years Experience</p>
-        </div>
-        <div className="  ">
-          <h2 className="text-h2 text-customColor-secondary font-extrabold">
-            12K+
-          </h2>
-          <p className="text-t2 font-semibold">Servive Closed</p>
-        </div>
-        <div className="  ">
-          <h2 className="text-h2 text-customColor-secondary font-extrabold">
-            100%
-          </h2>
-          <p className="text-t2 font-semibold">Customer Satisfaction</p>
-        </div>
+        {customerSatisfactionData.map((item, index) => {
+          return (
+            <div className="">
+              <h2 className="text-h2 text-customColor-secondary font-extrabold">
+                {item.value}
+              </h2>
+              <p className="text-t2 font-semibold">{item.label}</p>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
