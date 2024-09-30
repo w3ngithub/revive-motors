64b9e681f5e4d83d11e616f6a10d68d0008a3826
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-09-30 10:55:01 +0545

    feat: maintenance  section  made dynamic

--- src/components/custom-components/maintenance.tsx
+++ src/components/custom-components/maintenance.tsx
@@ -1,165 +1,45 @@
 "use client";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
+import cardData from "../../json/maintenance.json";
 
 const Maintenance = () => {
-  // min-w-[100vw]
+  const { smallHeading, largeHeading, cards } = cardData;
+
   return (
-    <section className="container flex flex-row justify-center min-h-[100vh] max-w-[100vw]   bg-customColor-lightGrey border border-red-500 ">
-      <div className="flex flex-col justify-center items-center gap-8  my-10">
-        <p className="text-t2 font-bold text-center p-1  ">What we Offer</p>
-        <h2 className="text-h2 font-extrabold max-w-[858px]  leading-tight text-center">
-          We offer full service auto repair & maintenance
+    <section className="container flex flex-row justify-center min-h-[100vh] max-w-[100vw] bg-customColor-lightGrey border border-red-500">
+      <div className="flex flex-col justify-center items-center gap-8 my-10">
+        <p className="text-t2 font-bold text-center p-1">{smallHeading}</p>
+        <h2 className="text-h2 font-extrabold max-w-[858px] leading-tight text-center">
+          {largeHeading}
         </h2>
-        <div className="flex flex-row justify-center items-center flex-wrap gap-2  w-[80%]">
-          <Card className=" group flex flex-col justify-center items-center   w-[316px] h-[232px] rounded-sm hover:bg-customColor-primary hover:cursor-pointer ">
-            <CardHeader>
-              <CardTitle className="flex justify-center items-center">
-                <Image
-                  src="/project/car-doctor.svg"
-                  width={44}
-                  height={49}
-                  className="group-hover:brightness-0 group-hover:invert "
-                  alt="Car Icon"
-                />
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p className="text-center text-t2 font-semibold  group-hover:text-customColor-white">
-                Diagnostics
-              </p>
-            </CardContent>
-          </Card>
-          {/* ///////////////////// */}
-          <Card className=" group flex flex-col justify-center items-center   w-[316px] h-[232px] rounded-sm hover:bg-customColor-primary  ">
-            <CardHeader>
-              <CardTitle className="flex justify-center items-center">
-                <Image
-                  src="/project/car-doctor.svg"
-                  width={44}
-                  height={49}
-                  className="group-hover:brightness-0 group-hover:invert "
-                  alt="Car Icon"
-                />
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p className="text-center text-t2 font-semibold  group-hover:text-customColor-white">
-                Diagnostics
-              </p>
-            </CardContent>
-          </Card>{" "}
-          <Card className=" group flex flex-col justify-center items-center   w-[316px] h-[232px] rounded-sm hover:bg-customColor-primary  ">
-            <CardHeader>
-              <CardTitle className="flex justify-center items-center">
-                <Image
-                  src="/project/car-doctor.svg"
-                  width={44}
-                  height={49}
-                  className="group-hover:brightness-0 group-hover:invert "
-                  alt="Car Icon"
-                />
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p className="text-center text-t2 font-semibold  group-hover:text-customColor-white">
-                Diagnostics
-              </p>
-            </CardContent>
-          </Card>{" "}
-          <Card className=" group flex flex-col justify-center items-center   w-[316px] h-[232px] rounded-sm hover:bg-customColor-primary  ">
-            <CardHeader>
-              <CardTitle className="flex justify-center items-center">
-                <Image
-                  src="/project/car-doctor.svg"
-                  width={44}
-                  height={49}
-                  className="group-hover:brightness-0 group-hover:invert "
-                  alt="Car Icon"
-                />
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p className="text-center text-t2 font-semibold  group-hover:text-customColor-white">
-                Diagnostics
-              </p>
-            </CardContent>
-          </Card>{" "}
-          <Card className=" group flex flex-col justify-center items-center   w-[316px] h-[232px] rounded-sm hover:bg-customColor-primary  ">
-            <CardHeader>
-              <CardTitle className="flex justify-center items-center">
-                <Image
-                  src="/project/car-doctor.svg"
-                  width={44}
-                  height={49}
-                  className="group-hover:brightness-0 group-hover:invert "
-                  alt="Car Icon"
-                />
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p className="text-center text-t2 font-semibold  group-hover:text-customColor-white">
-                Diagnostics
-              </p>
-            </CardContent>
-          </Card>{" "}
-          <Card className=" group flex flex-col justify-center items-center   w-[316px] h-[232px] rounded-sm hover:bg-customColor-primary  ">
-            <CardHeader>
-              <CardTitle className="flex justify-center items-center">
-                <Image
-                  src="/project/car-doctor.svg"
-                  width={44}
-                  height={49}
-                  className="group-hover:brightness-0 group-hover:invert "
-                  alt="Car Icon"
-                />
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p className="text-center text-t2 font-semibold  group-hover:text-customColor-white">
-                Diagnostics
-              </p>
-            </CardContent>
-          </Card>{" "}
-          <Card className=" group flex flex-col justify-center items-center   w-[316px] h-[232px] rounded-sm hover:bg-customColor-primary  ">
-            <CardHeader>
-              <CardTitle className="flex justify-center items-center">
-                <Image
-                  src="/project/car-doctor.svg"
-                  width={44}
-                  height={49}
-                  className="group-hover:brightness-0 group-hover:invert "
-                  alt="Car Icon"
-                />
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p className="text-center text-t2 font-semibold  group-hover:text-customColor-white">
-                Diagnostics
-              </p>
-            </CardContent>
-          </Card>{" "}
-          <Card className=" group flex flex-col justify-center items-center   w-[316px] h-[232px] rounded-sm hover:bg-customColor-primary  ">
-            <CardHeader>
-              <CardTitle className="flex justify-center items-center">
-                <Image
-                  src="/project/car-doctor.svg"
-                  width={44}
-                  height={49}
-                  className="group-hover:brightness-0 group-hover:invert "
-                  alt="Car Icon"
-                />
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p className="text-center text-t2 font-semibold  group-hover:text-customColor-white">
-                Diagnostics
-              </p>
-            </CardContent>
-          </Card>
-          <div className=" w-[84%] pt-2">
-            <div className=" flex flex-row items-center justify-end gap-6 hover:cursor-pointer">
-              <p className="text-b1 font-semibold">Know more about us </p>
+        <div className="flex flex-row justify-center items-center flex-wrap gap-2 w-[80%]">
+          {cards.map((card, index) => (
+            <Card
+              key={index}
+              className="group flex flex-col justify-center items-center w-[316px] h-[232px] rounded-sm hover:bg-customColor-primary hover:cursor-pointer"
+            >
+              <CardHeader>
+                <CardTitle className="flex justify-center items-center">
+                  <Image
+                    src={card.image}
+                    width={44}
+                    height={49}
+                    className="group-hover:brightness-0 group-hover:invert"
+                    alt={`${card.title} Icon`}
+                  />
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p className="text-center text-t2 font-semibold group-hover:text-customColor-white">
+                  {card.title}
+                </p>
+              </CardContent>
+            </Card>
+          ))}
+          <div className="w-[84%] pt-2">
+            <div className="flex flex-row items-center justify-end gap-6 hover:cursor-pointer">
+              <p className="text-b1 font-semibold">Know more about us</p>
               <span className="">
                 <Image
                   src="/project/right-arrow.svg"
