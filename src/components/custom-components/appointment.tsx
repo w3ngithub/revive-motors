4cc0727c1447c928d5d25ab5c692198626840f6d
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-09-30 10:45:55 +0545

    feat: appointment  section  made dynamic

--- src/components/custom-components/appointment.tsx
+++ src/components/custom-components/appointment.tsx
@@ -1,46 +1,28 @@
 import React from "react";
 import { Button } from "../ui/button";
 import Image from "next/image";
+import appointmentData from "../../json/appointment.json";
 
 const Appointment = () => {
+  const { leftContent, rightContent } = appointmentData;
   // min-w-[100vw]
   return (
     <section className="flex flex-row items-center justify-center min-h-[100vh] w-full overflow-hidden  border-2 border-red-700 bg-customColor-white">
       <div className="container grid sm:grid-cols-2 ">
         <div className="flex flex-col justify-center items-start border border-red-500 gap-11">
           <p className="text-h2 font-extrabold max-w-[624px] leading-tight">
-            We follow a clear process to help you out.
+          {leftContent.heading}
           </p>
           <p className="text-b2 text-customColor-grey max-w-[425px] border-2 border-red-700">
-            Through True Rich Attended does no end it his mother since real had
-            half every him case in packages enquire
+          {leftContent.description}
           </p>
           <Button className="bg-customColor-primary text-b1 text-customColor-white max-w-[177px] max-h-[64px] p-7 px-9 rounded-xl">
-            Learn more
+          {leftContent.button}
           </Button>
         </div>
         <div className="flex flex-col justify-center items-start border-2 border-slate-900">
           <div className="space-y-6 relative">
-            {[
-              {
-                number: "01",
-                title: "Get a Quote",
-                description:
-                  "Through True Rich Attended does no end it his mother since real had half every",
-              },
-              {
-                number: "02",
-                title: "Book an Appointment",
-                description:
-                  "Through True Rich Attended does no end it his mother since real had half every",
-              },
-              {
-                number: "03",
-                title: "Get your Service Done",
-                description:
-                  "Through True Rich Attended does no end it his mother since real had half every",
-              },
-            ].map((step, index) => (
+            {rightContent.steps.map((step, index) => (
               <div
                 key={index}
                 className="flex items-start space-x-4 relative border border-red-950"
