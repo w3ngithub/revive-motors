8c2b881068a6d7cf2a4f7c077d34f37f51df4424
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-09-30 10:33:21 +0545

    feat: service  section  made dynamic

--- src/components/custom-components/service-form.tsx
+++ src/components/custom-components/service-form.tsx
@@ -3,81 +3,49 @@ import React from "react";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { Input } from "@/components/ui/input";
 import { Button } from "../ui/button";
+import serviceFormData from "../../json/serviceForm.json";
 
 const ServiceForm = () => {
+  const { heading, services, knowMore, form } = serviceFormData;
   // min-w-[100vw]
   return (
     <section className=" flex flex-row items-center justify-center overflow-hidden  min-h-[100vh] w-full border-2 border-red-700 bg-customColor-steelGrey my-10">
       <div className="container flex flex-row items-start justify-center border-2 border-red-700 pt-3 gap-6 max-lg:flex-col my-10">
         <div className="flex flex-col items-start gap-[53px] border-2 border-red-700">
           <h2 className="text-h2 font-extrabold max-w-[624px] leading-tight">
-            We are taking car servicing seriously
+            {heading}
           </h2>
-          <div className="flex flex-row justify-center align-center gap-4 hover:cursor-pointer">
-            <div className="border border-red-500">
-              <Image
-                src="/project/tool-logo.svg"
-                width={47}
-                height={47}
-                // className="h-[30.94px] w-[30.94px]"
-                alt="Phone Icon"
-              />
-            </div>
-            <div className="max-w-[360px]">
-              <p className="text-t2 font-semibold">Convenient service</p>
-              <p className="text-b2 text-customColor-grey">
-                Through True Rich Attended does no end it his mother since real
-                had half every him.
-              </p>
-            </div>
-          </div>
-          {/* ///////////////////////////////// */}
-          <div className="flex flex-row justify-center align-center gap-4 hover:cursor-pointer">
-            <div className="border border-red-500">
-              <Image
-                src="/project/man-logo.svg"
-                width={47}
-                height={47}
-                // className="h-[30.94px] w-[30.94px]"
-                alt="Phone Icon"
-              />
-            </div>
-            <div className="max-w-[360px]">
-              <p className="text-t2 font-semibold">Convenient service</p>
-              <p className="text-b2 text-customColor-grey">
-                Through True Rich Attended does no end it his mother since real
-                had half every him.
-              </p>
-            </div>
-          </div>{" "}
-          <div className="flex flex-row justify-center align-center gap-4 hover:cursor-pointer">
-            <div className="border border-red-500">
-              <Image
-                src="/project/dollar-logo.svg"
-                width={47}
-                height={47}
-                // className="h-[30.94px] w-[30.94px]"
-                alt="Phone Icon"
-              />
-            </div>
-            <div className="max-w-[360px]">
-              <p className="text-t2 font-semibold">Convenient service</p>
-              <p className="text-b2 text-customColor-grey">
-                Through True Rich Attended does no end it his mother since real
-                had half every him.
-              </p>
-            </div>
-          </div>
-          {/* ////////////////////////////////////// */}
+          {services.map((service, index) => {
+            return (
+              <div className="flex flex-row justify-center align-center gap-4 hover:cursor-pointer">
+                <div className="border border-red-500">
+                  <Image
+                    src={service.iconSrc}
+                    width={47}
+                    height={47}
+                    // className="h-[30.94px] w-[30.94px]"
+                    alt={service.altText}
+                  />
+                </div>
+                <div className="max-w-[360px]">
+                  <p className="text-t2 font-semibold">{service.title}</p>
+                  <p className="text-b2 text-customColor-grey">
+                    {service.description}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+
           <div className="flex flex-row items-center gap-6 border border-red-500 ml-11 pl-2 hover:cursor-pointer">
-            <p className="text-b1 font-semibold">Know more about us </p>
+            <p className="text-b1 font-semibold">{knowMore.text} </p>
             <span>
               <Image
-                src="/project/right-arrow.svg"
+                src={knowMore.arrowIconSrc}
                 width={52}
                 height={0}
                 // className="h-[30.94px] w-[30.94px]"
-                alt="Right arrow"
+                alt={knowMore.altText}
               />
             </span>
           </div>
@@ -116,5 +84,3 @@ const ServiceForm = () => {
 };
 
 export default ServiceForm;
-
-///////////////////////////
