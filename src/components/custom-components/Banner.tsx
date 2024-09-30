8b7856edd814a520e5bd8ca5322071a74039a020
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-09-30 16:12:21 +0545

    feat: banner  page  responsiveness added

--- src/components/custom-components/Banner.tsx
+++ src/components/custom-components/Banner.tsx
@@ -5,11 +5,12 @@ import Middle from "./middle";
 
 const Banner = () => {
   // min-h-[100vh]
+  // max-lg
   return (
-    <section className=" max-w-[100vw] border border-red-500">
-      <div className="grid grid-cols-12 border-2 border-slate-950 w-full max-h-[502px]">
+    <section className=" max-w-[100vw] ">
+      <div className="grid grid-cols-12  w-full max-h-[502px] max-lg:grid-cols-2">
         {/* Left Image */}
-        <div className="col-span-2 border-4 border-green-500 relative">
+        <div className="block col-span-2 relative max-lg:hidden">
           <Image
             src="/project/large-carss.png"
             alt="Car Pic"
@@ -20,12 +21,12 @@ const Banner = () => {
         </div>
 
         {/* Middle Component */}
-        <div className="block m-0 p-0  justify-center items-center col-span-5 border-4 border-blue-500">
+        <div className="block m-0 p-0  justify-center items-center col-span-5  max-lg:col-span-1 ">
           <Middle />
         </div>
 
         {/* Right Image */}
-        <div className="col-span-5 border-4 border-purple-500 relative">
+        <div className="col-span-5 relative  max-lg:col-span-1">
           <Image
             src="/project/clips.png"
             alt="Car Pic"
--- src/components/custom-components/middle.tsx
+++ src/components/custom-components/middle.tsx
@@ -6,10 +6,10 @@ const Middle = () => {
     <section>
       <div className="bg-customColor-primary flex justify-center ">
         <div className="flex flex-col justify-center items-start gap-11 bg-customColor-primary p-10 ">
-          <p className="text-h3 text-customColor-white font-bold max-w-[569px] leading-tight max-lg:w-full">
+          <h3 className="text-h3 text-customColor-white font-bold max-w-[569px] leading-tight max-lg:w-full">
             Get in touch with us for your service related query
-          </p>
-          <Button className="bg-customColor-black text-b1 text-customColor-white max-w-[203px] max-h-[64px] p-7 px-9 rounded-xl">
+          </h3>
+          <Button className="bg-customColor-black text-b1 text-customColor-white max-w-[203px] max-h-[64px] p-7 px-9 rounded-xl ">
             Contact Us
           </Button>
         </div>
