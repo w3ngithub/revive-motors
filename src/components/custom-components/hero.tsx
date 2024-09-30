0aa3228d498b6f31aee9e8178f5da01be0e55748
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-09-30 10:21:30 +0545

    feat: hero section  made dynamic

--- src/components/custom-components/hero.tsx
+++ src/components/custom-components/hero.tsx
@@ -1,33 +1,37 @@
 import React from "react";
 import { Button } from "../ui/button";
 import Image from "next/image";
+import heroData from "../../json/hero.json";
+import { Heading } from "lucide-react";
 
 const Hero = () => {
+  const { heading, button, openHours, heroImage } = heroData;
+
   return (
     <section className="border border-red-500 ">
       <div className="container grid sm:grid-cols-2 ">
         <div className="flex flex-col justify-center items-start border border-red-500 gap-11">
           <p className="text-h1 font-extrabold max-w-[624px] leading-tight">
-            Get your vehicle service done online at one place
+            {heading}
           </p>
           <Button className="bg-customColor-primary text-b1 text-customColor-white max-w-[203px] max-h-[64px] p-7 px-9 rounded-xl">
-            Book a service
+            {button.text}
           </Button>
           <div className="flex flex-row justify-center align-center gap-4 hover:cursor-pointer">
             <div className="border border-red-500">
               <Image
-                src="/project/clock-icon.svg"
+                src={openHours.iconSrc}
                 width={40}
                 height={40}
                 // className="h-[30.94px] w-[30.94px]"
-                alt="Phone Icon"
+                alt={openHours.altText}
               />
             </div>
             <div>
-              <p className="text-caption text-customColor-grey">We are open</p>
-              <p className="text-b1 font-semibold">
-                Monday to Friday 9:00 AM to 10:00 AM
+              <p className="text-caption text-customColor-grey">
+                {openHours.caption}
               </p>
+              <p className="text-b1 font-semibold">{openHours.schedule}</p>
             </div>
           </div>
         </div>
@@ -39,8 +43,8 @@ const Hero = () => {
             height={632}
           /> */}
           <Image
-            src="/project/final.png"
-            alt="Hero icon"
+            src={heroImage.src}
+            alt={heroImage.altText}
             width={605}
             height={632}
           />
