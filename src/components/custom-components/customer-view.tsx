dc0475d14be9151bb3f49eb1209ce87b41ab0c86
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-09-30 12:02:28 +0545

    feat: customer-view  section made dynamic

--- src/components/custom-components/customer-view.tsx
+++ src/components/custom-components/customer-view.tsx
@@ -10,38 +10,16 @@ import {
 } from "@/components/ui/carousel";
 import { Card, CardContent } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-
-const testimonials = [
-  {
-    name: "Jonathan Vallem",
-    location: "New York",
-    content:
-      "I have got my car repaired at Finsweet many times before, they are good at what they do. Not only did they repair my car in a short comprehensive look on all the working",
-    avatar: "/project/profile.png",
-  },
-  {
-    name: "Smith Johnson",
-    location: "New York",
-    content:
-      "I have got my car repaired at Finsweet many times before, they are good at what they do. Not only did they repair my car in a short comprehensive look on all the working",
-    avatar: "/project/profile.png",
-  },
-  {
-    name: "Smith Johnson",
-    location: "New York",
-    content:
-      "I have got my car repaired at Finsweet many times before, they are good at what they do. Not only did they repair my car in a short comprehensive look on all the working",
-    avatar: "/project/profile.png",
-  },
-];
+import customerViewData from "../../json/customer-view.json";
 
 const CustomerView = () => {
+  const { heading, testimonials } = customerViewData;
   // border border-red-500
   return (
     <section className="bg-gray-100 py-16 px-4 sm:px-6 lg:px-8 min-h-[100vh] max-w-[100vw] ">
       <div className="container mx-auto max-w-7xl">
         <h3 className="text-h3 font-bold text-center mb-12 max-w-[909px] mx-auto">
-          Our customers say the nicest things about our service
+          {heading}
         </h3>
         <div className="flex flex-col justify-between w-full h-full  border border-red-500">
           {/* Carousel content */}
