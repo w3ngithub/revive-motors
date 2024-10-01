2ea0a52154d4fadcc8a61d07f4db74b4ed24a577
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-10-01 09:13:13 +0545

    fix: carasoul arrow fixed

--- src/components/custom-components/customer-view.tsx
+++ src/components/custom-components/customer-view.tsx
@@ -78,7 +78,8 @@ const CustomerView = () => {
             </CarouselContent>
 
             {/* Carousel navigation */}
-            <div className="flex justify-end gap-3 mt-4 border border-red-700">
+            {/* border border-red-700 */}
+            <div className="flex justify-end gap-3 mt-4 ">
               {/* <button
                 onClick={handlePrevious}
                 className="bg-gray-700 text-white px-4 py-2 rounded"
