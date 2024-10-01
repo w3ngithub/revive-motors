155cce657196e06789949e4ade5564a28a14b2a6
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-10-01 09:26:55 +0545

    feat: middleware added

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div>
+    <>
       
-    </div>
+    </>
   )
 }
---
+++ src/middleware.tsx
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request: NextResponse) {
+  console.log("middleware");
+
+  return NextResponse.redirect(new URL("/", request.url));
+}
+
+export const config = {
+  matcher: ["/home"], //yo yo path xa vane redirect garxa to "/login" page ma
+};
