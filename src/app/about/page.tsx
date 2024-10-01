cfb07329944cdd4b5b54dff598178115c8dddd3d
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-10-01 10:24:08 +0545

    feat: about-service page added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,11 @@
+import AboutService from '@/components/custom-components/about-service'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <AboutService />
+    </div>
   )
 }
 
