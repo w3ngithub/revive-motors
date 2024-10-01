a49cb3b04cb1a2a3f3d9bf8582f400f1d38cda69
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-10-01 10:59:36 +0545

    feat: about-process page UI comp

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,14 @@
-import AboutService from '@/components/custom-components/about-service'
-import React from 'react'
+import AboutProcess from "@/components/custom-components/about-process";
+import AboutService from "@/components/custom-components/about-service";
+import React from "react";
 
 const page = () => {
   return (
     <div>
       <AboutService />
+      <AboutProcess />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
