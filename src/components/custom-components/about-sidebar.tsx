8da9582fa0bb2f743594051e3b85a7cc1466a3c9
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-10-01 15:51:28 +0545

    fix: service page card fixed

--- src/components/custom-components/about-sidebar.tsx
+++ src/components/custom-components/about-sidebar.tsx
@@ -4,9 +4,9 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 const AboutSidebar = () => {
   return (
-    <div className="container flex flex-col justify-center items-center gap-3 mb-5 bg-customColor-lightGrey max-w-[328px] min-h-[670px]">
+    <div className="container flex flex-col justify-center items-center gap-3 mb-5 bg-customColor-lightGrey min-w-[100%] min-h-[670px]">
       <p className="text-t1 font-bold">Other services</p>
-      <Card className="group flex flex-col justify-center items-center w-[264px] h-[158px] rounded-sm hover:bg-customColor-primary hover:cursor-pointer">
+      <Card className="group flex flex-col justify-center items-center max-w-[90%] h-[158px] rounded-sm hover:bg-customColor-primary hover:cursor-pointer">
         <CardHeader>
           <CardTitle className="flex justify-center items-center">
             <Image
@@ -24,7 +24,7 @@ const AboutSidebar = () => {
           </p>
         </CardContent>
       </Card>
-      <Card className="group flex flex-col justify-center items-center w-[264px] h-[158px] rounded-sm hover:bg-customColor-primary hover:cursor-pointer">
+      <Card className="group flex flex-col justify-center items-center max-w-[90%] h-[158px] rounded-sm hover:bg-customColor-primary hover:cursor-pointer">
         <CardHeader>
           <CardTitle className="flex justify-center items-center">
             <Image
@@ -42,7 +42,7 @@ const AboutSidebar = () => {
           </p>
         </CardContent>
       </Card>
-      <Card className="group flex flex-col justify-center items-center w-[264px] h-[158px] rounded-sm hover:bg-customColor-primary hover:cursor-pointer">
+      <Card className="group flex flex-col justify-center items-center max-w-[90%] h-[158px] rounded-sm hover:bg-customColor-primary hover:cursor-pointer">
         <CardHeader>
           <CardTitle className="flex justify-center items-center">
             <Image
