9ce28da78357b29fca7c6312de9015b367c8291d
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-10-02 12:11:06 +0545

    feat:blog-home added

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,12 @@
+import BlogHome from "@/components/custom-components/blog-home";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <BlogHome />
+    </>
+  );
+};
+
+export default page;
