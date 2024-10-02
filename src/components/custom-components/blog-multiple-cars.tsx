1ed65a9714658c38e102244d4657d9958cdc2286
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-10-02 14:53:49 +0545

    feat: first card comp

--- src/components/custom-components/blog-multiple-cars.tsx
+++ src/components/custom-components/blog-multiple-cars.tsx
@@ -21,42 +21,21 @@ const BlogMultipleCars = () => {
                     height={266}
                   />
                 </a>
-                <div className="p-5  flex flex-col gap-4 2xl:pl-4 ">
-                  <span className="text-b2 text-customColor-grey font-semibold 2xl:pl-4">
+                <div className="my-5  flex flex-col gap-4  max-2xl:pl-3     ">
+                  <span className="text-b2 text-customColor-grey font-semibold      ">
                     Posted on October 6th 2021
                   </span>
                   <a href="#">
                     {/* 375px */}
-                    <h4 className="mb-2 text-t1 font-bold tracking-tight text-gray-900 dark:text-white 2xl:pl-4      2xl:max-w-[375px]">
+                    <h4 className="mb-2 text-t1 font-bold tracking-tight text-gray-900 dark:text-white        2xl:max-w-[375px]">
                       5 Genius Car Accessories You Should Never Drive Without
                     </h4>
                   </a>
-                  <p className=" mb-3 text-b2 font-semibold text-customColor-grey leading-relaxed 2xl:pl-4 max-w-[405px]    ">
+                  <p className=" mb-3 text-b2 font-semibold text-customColor-grey leading-relaxed       max-w-[405px]    ">
                     We provide a full range of front end mechanical repairs for
                     all makes and models We provide a full range of front end
                     mechanical repairs for all makes and models
                   </p>
-                  <a
-                    href="#"
-                    className="text-b1 font-semibold inline-flex items-center px-3 py-2 text-sm  text-left rounded-lg focus:ring-4 focus:outline-none 2xl:pl-4"
-                  >
-                    Read more
-                    <svg
-                      className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 14 10"
-                    >
-                      <path
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M1 5h12m0 0L9 1m4 4L9 9"
-                      />
-                    </svg>
-                  </a>
                 </div>
               </div>
             </div>
