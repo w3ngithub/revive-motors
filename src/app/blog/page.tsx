eb9a6839bd3c399182734e9cbed00161331fe9a7
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-10-02 14:39:22 +0545

    feat:blog-multiple-cars page added

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,6 @@
 import BlogCard from "@/components/custom-components/blog-card";
 import BlogHome from "@/components/custom-components/blog-home";
+import BlogMultipleCars from "@/components/custom-components/blog-multiple-cars";
 import React from "react";
 
 const page = () => {
@@ -7,6 +8,7 @@ const page = () => {
     <>
       <BlogHome />
       <BlogCard />
+      <BlogMultipleCars />
     </>
   );
 };
