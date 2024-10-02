1d84538d26c609e06eb417c2ef594e3c093827a6
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-10-02 09:49:26 +0545

    feat:customer-satisfaction page added

--- src/components/custom-components/about-card.tsx
+++ src/components/custom-components/about-card.tsx
@@ -2,88 +2,92 @@ import React from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
 import cardData from "../../json/maintenance.json";
+import CustomerSatisfaction from "./customer-satisfaction";
 
 const AboutCard = () => {
   const { smallHeading, largeHeading, cards } = cardData;
 
   return (
-    <div className="bg-customColor-lightGrey ">
-      <section className="container flex flex-row justify-center min-h-[100vh] max-w-[100vw] bg-customColor-lightGrey   ">
-        <div className="   flex flex-col justify-center items-center gap-[64px] my-10">
-          <h2 className="text-h2 font-extrabold max-w-[858px] leading-tight text-center">
-            Services we provide to our valued customers
-          </h2>
-          <div className="container flex flex-row justify-center items-center flex-wrap gap-2 w-[100%%] max-2xl:w-[100%]  ">
-            <Card className="group flex flex-col justify-center items-center w-[425px] h-[424px] rounded-none  hover:cursor-pointer    max-sm:w-[300px]">
-              <CardHeader>
-                <CardTitle className="flex justify-center items-center">
-                  <Image
-                    src="/project/about-tool-logo.svg"
-                    width={64}
-                    height={64}
-                    className=""
-                    alt="Diagnostics"
-                  />
-                </CardTitle>
-              </CardHeader>
-              <CardContent className="  flex flex-col justify-center items-center gap-4">
-                <p className="text-center text-t1 font-bold  ">
-                  Convenient Service
-                </p>
-                <p className="text-b1 text-customColor-grey max-w-[295px] min-h-[96px]    ">
-                  Through True Rich Attended does no end it his mother since
-                  real had half every.
-                </p>
-              </CardContent>
-            </Card>
-            <Card className="group flex flex-col justify-center items-center w-[425px] h-[424px] rounded-none  hover:cursor-pointer    max-sm:w-[300px]">
-              <CardHeader>
-                <CardTitle className="flex justify-center items-center">
-                  <Image
-                    src="/project/about-tool-logo.svg"
-                    width={64}
-                    height={64}
-                    className=""
-                    alt="Diagnostics"
-                  />
-                </CardTitle>
-              </CardHeader>
-              <CardContent className="  flex flex-col justify-center items-center gap-4">
-                <p className="text-center text-t1 font-bold  ">
-                  Convenient Service
-                </p>
-                <p className="text-b1 text-customColor-grey max-w-[295px] min-h-[96px]    ">
-                  Through True Rich Attended does no end it his mother since
-                  real had half every.
-                </p>
-              </CardContent>
-            </Card>
-            <Card className="group flex flex-col justify-center items-center w-[425px] h-[424px] rounded-none  hover:cursor-pointer    max-sm:w-[300px]">
-              <CardHeader>
-                <CardTitle className="flex justify-center items-center">
-                  <Image
-                    src="/project/about-tool-logo.svg"
-                    width={64}
-                    height={64}
-                    className=""
-                    alt="Diagnostics"
-                  />
-                </CardTitle>
-              </CardHeader>
-              <CardContent className="  flex flex-col justify-center items-center gap-4">
-                <p className="text-center text-t1 font-bold  ">
-                  Convenient Service
-                </p>
-                <p className="text-b1 text-customColor-grey max-w-[295px] min-h-[96px]    ">
-                  Through True Rich Attended does no end it his mother since
-                  real had half every.
-                </p>
-              </CardContent>
-            </Card>
+    <>
+      <div className="bg-customColor-lightGrey ">
+        <section className="container flex flex-row justify-center min-h-[100vh] max-w-[100vw] bg-customColor-lightGrey   ">
+          <div className="   flex flex-col justify-center items-center gap-[64px] my-10">
+            <h2 className="text-h2 font-extrabold max-w-[858px] leading-tight text-center">
+              Services we provide to our valued customers
+            </h2>
+            <div className="container flex flex-row justify-center items-center flex-wrap gap-2 w-[100%%] max-2xl:w-[100%]  ">
+              <Card className="group flex flex-col justify-center items-center w-[425px] h-[424px] rounded-none  hover:cursor-pointer    max-sm:w-[300px]">
+                <CardHeader>
+                  <CardTitle className="flex justify-center items-center">
+                    <Image
+                      src="/project/about-tool-logo.svg"
+                      width={64}
+                      height={64}
+                      className=""
+                      alt="Diagnostics"
+                    />
+                  </CardTitle>
+                </CardHeader>
+                <CardContent className="  flex flex-col justify-center items-center gap-4">
+                  <p className="text-center text-t1 font-bold  ">
+                    Convenient Service
+                  </p>
+                  <p className="text-b1 text-customColor-grey max-w-[295px] min-h-[96px]    ">
+                    Through True Rich Attended does no end it his mother since
+                    real had half every.
+                  </p>
+                </CardContent>
+              </Card>
+              <Card className="group flex flex-col justify-center items-center w-[425px] h-[424px] rounded-none  hover:cursor-pointer    max-sm:w-[300px]">
+                <CardHeader>
+                  <CardTitle className="flex justify-center items-center">
+                    <Image
+                      src="/project/about-tool-logo.svg"
+                      width={64}
+                      height={64}
+                      className=""
+                      alt="Diagnostics"
+                    />
+                  </CardTitle>
+                </CardHeader>
+                <CardContent className="  flex flex-col justify-center items-center gap-4">
+                  <p className="text-center text-t1 font-bold  ">
+                    Convenient Service
+                  </p>
+                  <p className="text-b1 text-customColor-grey max-w-[295px] min-h-[96px]    ">
+                    Through True Rich Attended does no end it his mother since
+                    real had half every.
+                  </p>
+                </CardContent>
+              </Card>
+              <Card className="group flex flex-col justify-center items-center w-[425px] h-[424px] rounded-none  hover:cursor-pointer    max-sm:w-[300px]">
+                <CardHeader>
+                  <CardTitle className="flex justify-center items-center">
+                    <Image
+                      src="/project/about-tool-logo.svg"
+                      width={64}
+                      height={64}
+                      className=""
+                      alt="Diagnostics"
+                    />
+                  </CardTitle>
+                </CardHeader>
+                <CardContent className="  flex flex-col justify-center items-center gap-4">
+                  <p className="text-center text-t1 font-bold  ">
+                    Convenient Service
+                  </p>
+                  <p className="text-b1 text-customColor-grey max-w-[295px] min-h-[96px]    ">
+                    Through True Rich Attended does no end it his mother since
+                    real had half every.
+                  </p>
+                </CardContent>
+              </Card>
+            </div>
           </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
+      <CustomerSatisfaction />
+    </>
   );
 };
 
