9d0b65bb75d26c94890324d32c8d9a721a327e5a
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-10-02 13:36:10 +0545

    feat: fix on 1st container

--- src/components/custom-components/blog-card.tsx
+++ src/components/custom-components/blog-card.tsx
@@ -4,34 +4,39 @@ const BlogCard = () => {
   return (
     <section className="bg-[#f5f5f5]  min-h-[100vh] max-w-[100vw] flex flex-row justify-center items-center ">
       <div className="container ">
-        <div className="grid lg:grid-cols-12 border border-red-500 h-[746px]">
-          <div className="col-span-5 border border-red-500">
+        <div className="grid lg:grid-cols-12 border border-red-500 max-h-[746px]">
+          <div className="col-span-5 border border-green-500  ">
             {/* /////////////////////// */}
-            <div className="max-w-[624px] min-h-[710px] bg-white border border-gray-200 rounded-lg shadow dark:border-gray-700 flex flex-col justify-center items-center">
+            <div className="max-w-[537px] min-h-[746px] bg-white border border-gray-200 rounded-lg shadow dark:border-gray-700 flex flex-col justify-start items-center">
               <a href="#">
+                {/*  */}
                 <img
-                  className="rounded-t-lg"
-                  src="/project/about-diagnostics.png"
+                  className="rounded-none border border-red-500 mt-4"
+                  src="/project/blog-car-card.png"
                   alt="Auto Diagnostics"
+                  width={473}
+                  height={330}
                 />
               </a>
-              <div className="p-5">
+              <div className="p-5 border border-green-500 flex flex-col gap-4">
+                <span className="text-b2 text-customColor-grey font-semibold">
+                  Posted on October 6th 2021
+                </span>
                 <a href="#">
-                  <h5 className="mb-2 text-h3 font-bold tracking-tight text-gray-900 dark:text-white">
-                    Auto Diagnostics
-                  </h5>
+                  <h4 className="mb-2 text-h4 font-bold tracking-tight text-gray-900 dark:text-white border border-red-500">
+                    Should I Buy a New Car or Lease a New Car in 2021?
+                  </h4>
                 </a>
-                <p className="max-w-[500px] mb-3 text-t2 font-normal text-customColor-grey ">
-                  Through True Rich Attended does no end it his mother since
-                  favourable real had half every him case in packages enquire we
-                  up ecstatic. Through True Rich Attended does no end it his
-                  mother.
+                <p className="max-w-[500px] mb-3 text-b2 font-semibold text-customColor-grey leading-relaxed">
+                  We provide a full range of front end mechanical repairs for
+                  all makes and models of cars, no matter the cause. This
+                  includes, We provide a full range of front end mechanical.
                 </p>
                 <a
                   href="#"
-                  className="text-b1 font-semibold inline-flex items-center px-3 py-2 text-sm  text-center rounded-lg focus:ring-4 focus:outline-none"
+                  className="text-b1 font-semibold inline-flex items-center px-3 py-2 text-sm  text-left rounded-lg focus:ring-4 focus:outline-none"
                 >
-                  See service details
+                  Read more
                   <svg
                     className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
                     aria-hidden="true"
