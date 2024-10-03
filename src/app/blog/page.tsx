9f05b9e8d6e59dd2a4bdb66f238931bf95745f3c
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-10-03 11:35:36 +0545

    feat: banner added

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,6 +4,7 @@
 import BlogCard from "@/components/modules/blog/blog-card";
 import BlogHome from "@/components/modules/blog/blog-home";
 import BlogMultipleCars from "@/components/modules/blog/blog-multiple-cars";
+import Banner from "@/components/modules/common/banner";
 import React from "react";
 
 const page = () => {
@@ -13,6 +14,7 @@ const page = () => {
         <BlogHome />
         <BlogCard />
         <BlogMultipleCars />
+        <Banner />
       </div>
     </>
   );
