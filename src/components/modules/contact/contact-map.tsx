6b600ed5a17b933a316248c0647b622e70709fab
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-10-03 13:18:49 +0545

    feat: map layout

--- src/components/modules/contact/contact-map.tsx
+++ src/components/modules/contact/contact-map.tsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const ContactMap = () => {
   return (
-    <section>
-        <div>
-            
+    <section className="min-h-[100vh] w-full ">
+      <div className="border border-red-500 ">
+        <div className="border border-red-500 h-[512px] ">
+          <div className="w-[1280] h-[512px]">
+            <Image
+              src="/images/map.png"
+              alt="Map"
+              //   layout="fill"
+              //   objectFit="cover"
+              className="w-full h-full"
+              width={1280}
+              height={512}
+            />
+          </div>
         </div>
+        <div className="border border-red-500 h-[448px]">
+          <div className="w-[1280] h-[512px]">
+            <Image
+              src="/images/blog-map-cover.png"
+              alt="Map"
+              //   layout="fill"
+              //   objectFit="cover"
+              className="w-full h-full"
+              width={1280}
+              height={512}
+            />
+          </div>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default ContactMap
+export default ContactMap;
