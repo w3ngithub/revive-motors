24ed92caec03582ba3fd26769a583c6cb10095a1
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-10-03 12:09:09 +0545

    feat: contact form comp

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import ContactForm from "@/components/modules/contact/contact-form";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <ContactForm />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/modules/contact/contact-form.tsx
@@ -0,0 +1,74 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import React from "react";
+
+const ContactForm = () => {
+  return (
+    <>
+      <section className="flex flex-row items-center justify-center min-h-[100vh] w-full overflow-hidden   bg-customColor-white ">
+        <div className="container grid grid-cols-1 lg:grid-cols-2 border border-red-500 w-[1244px] h-[628px]">
+          <div className="border border-red-500 ">
+            {" "}
+            <div className="">
+              <div className="flex flex-col gap-6">
+                <h1 className="text-h1 max-w-[507px]  border border-green-500 font-extrabold leading-tight max-lg:mt-7 max-sm:font-extrabold">
+                  Get in touch with our experts
+                </h1>
+                <form className="flex flex-col flex-wrap gap-1 border border-red-500">
+                  {/* sm:w-[480px] md:w-[540px] lg:w-[624px] */}
+                  <Input
+                    className="h-[72px] w-full  rounded-sm bg-customColor-lightGrey text-b2 text-customColor-black font-semibold"
+                    placeholder="Your Full Name"
+                  />
+                  <Input
+                    className="h-[72px] w-full  rounded-sm bg-customColor-lightGrey text-b2 text-customColor-black font-semibold"
+                    placeholder="Your Email"
+                  />
+                  <Input
+                    className="h-[72px] w-full  rounded-sm bg-customColor-lightGrey text-b2 text-customColor-black font-semibold"
+                    placeholder="Select service type"
+                  />
+                  <Input
+                    className="h-[72px] w-full  rounded-sm bg-customColor-lightGrey text-b2 text-customColor-black font-semibold"
+                    placeholder="Message"
+                  />
+                  <Button className="bg-customColor-primary text-b1 text-customColor-white max-w-[203px] max-h-[64px] p-7 px-9 rounded-xl mt-5">
+                    Send Message
+                  </Button>
+                </form>
+              </div>
+            </div>
+          </div>
+          <div className="border border-red-500">
+            <div className="flex flex-col gap-5 border border-red-500">
+              <div>
+                <p className="text-b2 mb-1 text-customColor-grey">Address</p>
+                <p className="text-t2 font-semibold max-sm:text-b1 text-customColor-black">
+                  NH 234 Public Square San Francisco 65368
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col gap-5 border border-red-500">
+              <div>
+                <p className="text-b2 mb-1 text-customColor-grey">Address</p>
+                <p className="text-t2 font-semibold max-sm:text-b1 text-customColor-black">
+                  NH 234 Public Square San Francisco 65368
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col gap-5 border border-red-500">
+              <div>
+                <p className="text-b2 mb-1 text-customColor-grey">Address</p>
+                <p className="text-t2 font-semibold max-sm:text-b1 text-customColor-black">
+                  NH 234 Public Square San Francisco 65368
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default ContactForm;
