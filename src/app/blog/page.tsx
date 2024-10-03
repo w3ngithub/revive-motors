2b94081907f65b4440da5962248be7cb3a9c0491
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-10-03 10:08:12 +0545

    refactor: blog page refactor completed

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,16 +1,19 @@
-import BlogCard from "@/components/custom-components/blog-card";
-import BlogHome from "@/components/custom-components/blog-home";
-import BlogMultipleCars from "@/components/custom-components/blog-multiple-cars";
+// import BlogCard from "@/components/custom-components/blog-card";
+// import BlogHome from "@/components/custom-components/blog-home";
+// import BlogMultipleCars from "@/components/custom-components/blog-multiple-cars";
+import BlogCard from "@/components/modules/blog/blog-card";
+import BlogHome from "@/components/modules/blog/blog-home";
+import BlogMultipleCars from "@/components/modules/blog/blog-multiple-cars";
 import React from "react";
 
 const page = () => {
   return (
     <>
-      {/* <div>
+      <div>  
         <BlogHome />
         <BlogCard />
         <BlogMultipleCars />
-      </div> */}
+      </div>
     </>
   );
 };
--- src/components/modules/blog/blog-home.tsx
+++ src/components/modules/blog/blog-home.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import { Button } from "../ui/button";
+import { Button } from "../../ui/button";
 
 const BlogHome = () => {
   return (
