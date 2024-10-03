441ae9b70eba550a783c4bed4e953c8e54dc4387
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-10-03 10:36:10 +0545

    feat: blog-card completed

--- src/components/modules/blog/blog-card.tsx
+++ src/components/modules/blog/blog-card.tsx
@@ -5,13 +5,13 @@ const BlogCard = () => {
   return (
     // min-h-[100vh]
     <section className="bg-[#f5f5f5] min-h-screen max-w-[100vw] flex flex-row justify-center items-center pb-10  ">
-      <div className="container  ">
+      <div className="container  max-2xl:my-10 ">
         {/* max-h-[746px] */}
-        <div className="grid lg:grid-cols-12 border border-red-500">
-          <div className="col-span-5 border border-red-500  ">
+        <div className="grid lg:grid-cols-12 border border-red-500 space-x-10 space-y-10">
+          <div className="col-span-5 border border-red-500">
             {/* /////////////////////// */}
             {/* min-h-[746px] */}
-            <div className="max-w-[537px]  bg-white border border-gray-200 rounded-none shadow dark:border-gray-700 flex flex-col justify-start items-center ">
+            <div className="max-w-[537px]  bg-white border border-gray-200 rounded-none shadow  flex flex-col justify-start items-center ">
               <a href="#">
                 {/*  */}
                 <img
@@ -27,7 +27,7 @@ const BlogCard = () => {
                   Posted on October 6th 2021
                 </span>
                 <a href="#">
-                  <h4 className="mb-2 text-h4 font-bold tracking-tight text-gray-900 dark:text-white     2xl:pl-4">
+                  <h4 className="mb-2 text-h4 font-bold tracking-tight text-gray-900 dark:text-white   max-2xl:leading-tight  2xl:pl-4">
                     Should I Buy a New Car or Lease a New Car in 2021?
                   </h4>
                 </a>
@@ -67,26 +67,26 @@ const BlogCard = () => {
 
             <a
               href="#"
-              className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+              className="flex flex-col items-center bg-[#f5f5f5]  md:flex-row md:max-w-xl hover:bg-gray-100"
             >
               <img
-                className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-                src="/docs/images/blog/image-4.jpg"
-                alt=""
+                className="object-cover w-full h-96 md:h-auto md:w-48 rounded-none "
+                src="/images/blog-car-back.png"
+                alt="Car back"
               />
               <div className="flex flex-col justify-between p-4 leading-normal">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  Noteworthy technology acquisitions 2021
+                <h5 className="mb-2 text-t1 font-bold tracking-tight text-gray-900 max-2xl:leading-tight">
+                  Get an Awesome Number Plate for Your New Car
                 </h5>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                  Here are the biggest enterprise technology acquisitions of
-                  2021 so far, in reverse chronological order.
+                <p className="mb-3 text-b2 font-semibold text-gray-700 ">
+                  Posted on October 6th 2021
                 </p>
               </div>
             </a>
 
             {/* ////////////////// */}
           </div>
+          
         </div>
       </div>
     </section>
