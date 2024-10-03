082aa4b763dc71642ed6127f983ad3995df180bc
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-10-03 14:11:34 +0545

    feat: banner added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,20 +1,19 @@
-
 import AboutCard from "@/components/modules/about-us/about-card";
 import AboutQuote from "@/components/modules/about-us/about-quote";
+import Banner from "@/components/modules/common/banner";
 import OurTeam from "@/components/modules/our-team/our-team";
 import ServiceHero from "@/components/modules/services/service-hero";
 import React from "react";
 
 const page = () => {
-  return ( 
-   
+  return (
     <div>
       <ServiceHero />
       <AboutQuote />
       <AboutCard />
       {/* <CustomerSatisfaction /> */}
       <OurTeam />
-      {/* <Banner /> */}
+      <Banner />
     </div>
     // <div>
     //   {/* <AboutDiagnostics /> */}
