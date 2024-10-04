f0c89ebfe3faa4c4b1cc67fc25eccfecda7f3dde
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-10-04 12:41:54 +0545

    feat: blog-details-home page responsiveness added

--- src/components/modules/blog/blog-detail/blog-detail-home.tsx
+++ src/components/modules/blog/blog-detail/blog-detail-home.tsx
@@ -3,15 +3,15 @@ import React from "react";
 
 const BlogDetailHome = () => {
   return (
-    <section className="container border  border-red-500">
-      <article className=" max-w-4xl mx-auto px-4 py-8 border border-red-500">
+    <section className="container">
+      <article className=" max-w-4xl mx-auto px-4 py-8">
         <div className="mb-4 text-b2 text-customColor-grey font-semibold">
           Posted on October 6th 2021
         </div>
-        <h3 className="text-h3 font-bold mb-4">
+        <h3 className="text-h3 font-bold mb-4 max-2xl:max-w-[750px] max-2xl:leading-tight">
           Should I Buy a New Car or Lease a New Car in 2021?
         </h3>
-        <p className="text-b2 font-semibold text-customColor-grey mb-8">
+        <p className="text-b2 font-semibold text-customColor-grey mb-8 max-2xl:max-w-[750px] ">
           We provide a full range of front end mechanical repairs for all makes
           and models of cars, no matter the cause. This includes: We provide a
           full range of front end mechanical
