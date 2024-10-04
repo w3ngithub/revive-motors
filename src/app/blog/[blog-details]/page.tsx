72c965955d1da7b35a80ee54f9b83110a101a4d6
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-10-04 12:01:22 +0545

    feat: blog-details page added

---
+++ src/app/blog/[blog-details]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        blog-details
+    </div>
+  )
+}
+
+export default page
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -6,11 +6,6 @@ import React from "react";
 
 const page = () => {
   return (
-    // <>
-    //   {/* <AboutSidebar />
-    //   <AboutSidebarSecond /> */}
-    //   {/* <ServicesDiagnostics /> */}
-    // </>
     <div>
       <AboutService />
       <AboutDiagnostics />
