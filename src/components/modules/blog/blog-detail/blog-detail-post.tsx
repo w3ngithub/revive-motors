c273059c0668fc24f8b5f84e53edd16005638adf
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-10-04 13:21:20 +0545

    fix: blog-detail-post content fixed

--- src/components/modules/blog/blog-detail/blog-detail-post.tsx
+++ src/components/modules/blog/blog-detail/blog-detail-post.tsx
@@ -4,8 +4,8 @@ import React from "react";
 const BlogDetailPost = () => {
   //////////////////////
   return (
-    <section className="container  mb-11  border border-red-500">
-      <div className=" max-w-4xl mx-auto   border border-green-500">
+    <section className="container  mb-11">
+      <div className=" max-w-4xl mx-auto">
         <div className="space-y-10">
           {/* <div className="relative border border-green-800 w-[full] h-[334px]">
             <Image
@@ -19,10 +19,7 @@ const BlogDetailPost = () => {
             />
           </div> */}
           <div className="flex flex-col gap-7">
-            <h4 className="text-h4 font-bold">
-              The warning lights on your dashboard are the most obvious starting
-              point.
-            </h4>
+            <h4 className="text-h4 font-bold">This is a blog post headline</h4>
             <p className="text-b2 font-semibold text-customColor-grey leading-relaxed ">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
               dignissim dolor et dui bibendum aliquam. Mauris a quam sit amet
@@ -35,15 +32,18 @@ const BlogDetailPost = () => {
               convallis.
             </p>
             <p className="text-b2 font-semibold text-customColor-grey leading-relaxed">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-              dignissim dolor et dui bibendum aliquam. Mauris a quam sit amet
-              dui auctor dictum eget a elit. Pellentesque varius diam risus, ut
-              condimentum lorem volutpat vel. Nam vel orci pharetra eros
-              pulvinar cursus nec quis tellus. Quisque feugiat tortor lectus,
-              pretium interdum justo tincidunt a. Donec at congue lectus. Nulla
-              facilisi. Phasellus consectetur sapien accumsan lectus tincidunt
-              placerat. Etiam ornare nibh vel dui egestas, eu posuere metus
-              convallis.
+              Ut non urna a odio condimentum dictum. Proin egestas erat a orci
+              ultrices, vitae bibendum libero posuere. Quisque laoreet tincidunt
+              justo. Vestibulum congue dictum libero finibus vehicula. Aliquam
+              nisi velit, ultricies eget enim vel, venenatis mollis ante.
+              Maecenas sodales tristique quam. Suspendisse fringilla massa vel
+              dolor ornare rhoncus. Nullam ut orci mattis leo varius laoreet sed
+              mollis dui. Aenean placerat nec enim ut finibus. Maecenas suscipit
+              nibh eu neque egestas, non condimentum mi bibendum. Sed est eros,
+              molestie consectetur auctor non, lobortis quis tortor. Nam cursus
+              imperdiet massa volutpat hendrerit. Sed suscipit ligula iaculis
+              lorem sagittis tincidunt. Etiam pellentesque metus vel enim
+              iaculis aliquam. Mauris at nisi sed elit gravida malesuada.
             </p>
           </div>
           {/* /////////// */}
