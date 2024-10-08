7fa754f6cc906e6a346414f90073fe8aa7d72f2d
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-10-08 09:50:09 +0545

    fix: link fixed at button

--- src/components/modules/common/middle.tsx
+++ src/components/modules/common/middle.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Button } from "../../ui/button";
 
 const Middle = () => {
@@ -8,9 +9,11 @@ const Middle = () => {
           <h3 className="max-w-[569px] text-h3 font-bold leading-tight text-customColor-white max-lg:w-full">
             Get in touch with us for your service related query
           </h3>
-          <Button className="max-h-[64px] max-w-[203px] rounded-xl bg-customColor-black p-7 px-9 text-b1 text-customColor-white">
-            Contact Us
-          </Button>
+          <Link href="/contact">
+            <Button className="max-h-[64px] max-w-[203px] rounded-xl bg-customColor-black p-7 px-9 text-b1 text-customColor-white">
+              Contact Us
+            </Button>
+          </Link>
         </div>
       </div>
     </section>
