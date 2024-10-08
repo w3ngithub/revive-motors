9356ab623e003881db79a193290643d2d2c0b075
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-10-08 13:14:44 +0545

    fix: document icon fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="shortcut icon" href="#"></link>
+        <link
+          rel="shortcut icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22></text></svg>"
+        ></link>
       </head>
       <body className={`${manrope.className} flex min-h-screen flex-col`}>
         <Header />
